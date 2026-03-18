119bb0ea08b3056b73d57a9048d1441213ffc443
voidnull <void.null.t@gmail.com>
2026-03-18 21:59:51 +0800

    修复bug：IsVisible属性不支持

--- src/core/control_operator.py
+++ src/core/control_operator.py
@@ -288,7 +288,7 @@ class UiaOperator(BaseOperator):
                     control_info.rect = (rect.left, rect.top, rect.right, rect.bottom)
                 
                 control_info.is_enabled = uia_ctrl.IsEnabled
-                control_info.is_visible = uia_ctrl.IsVisible
+                control_info.is_visible = uia_ctrl.IsOffscreen == False
                 return control_info
             
         except Exception as e:
